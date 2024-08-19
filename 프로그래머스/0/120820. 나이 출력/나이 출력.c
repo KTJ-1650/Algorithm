a9cc75b76f2d54f513e5667b3ca0a33a64033903
KTJ-1650 <xowns1650@naver.com>
2024-08-20 08:31:37 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer = 0;
+    
+    int born= 2022;
+    
+    answer= (born+1)-age;
+    
+    return answer;
+}
